5a2334c0ea561b7e05214027f482679755434f6a
mmlumba <mmlumba@gmail.com>
2018-03-10 21:34:08 -0500

    add some semblance of front end

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const server = express();
+const path = require('path');
 const fs = require('fs');
 
-
+server.use(express.static(path.join(__dirname, 'public')));
 server.get('/', (request, response) => {
+    response.render('index.html', function(err, html) {
+        response.send(html);
+    });
+})
+server.get('/resume', (request, response) => {
     const file = fs.readFileSync("./resume.json")
     response.send(JSON.parse(file));
 })
 
-// port: differentiates between different traffic on same host
 server.listen(3000, () => console.log("marian\'s resume"))
