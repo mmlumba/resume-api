98421b015e31b0537f0962a95c4ccd816187c1eb
mmlumba <mmlumba@gmail.com>
2018-03-16 11:29:33 -0400

    add section for print only view

--- public/index.js
+++ public/index.js
@@ -1,2 +1,6 @@
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
 let curlUrlId = document.getElementById("curl-url");
 curlUrlId.innerHTML = window.location.href + "resume";
+email.textContent = "some@email.com"; // where env var goes later
+phone.textContent = "123-456-7899"; // where env var goes later
